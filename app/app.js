d21c67fb86f799b6328e2be5b241644ef575b3b6
RomaDemiurg <demiurgru@yandex.ru>
2016-07-16 01:59:03 +0300

    ui.bootstrap added

--- app/app.js
+++ app/app.js
@@ -1,29 +1,29 @@
 (function(){
     angular.module('TimeWaste', ['ui.bootstrap', 'ui.router', 'ngFileUpload'])
-            .config(function($stateProvider, $urlRouterProvider){
+        .config(function($stateProvider, $urlRouterProvider) {
             
             $urlRouterProvider.otherwise('/');
         
             $stateProvider
                 .state('signUp', {
-                url: "/signup",
-                templateUrl: "app/signup/signup.html",
-                controller: "SignupController"
-            })
+                    url: "/signup",
+                    templateUrl: "app/signup/signup.html",
+                    controller: "SignupController"
+                })
                 .state('editProfile', {
-                url: "/edit-profile",
-                templateUrl: "app/profile/edit-profile-view.html",
-                controller: "EditProfileController"
-            })
-              .state('main', {
-                url: "/",
-                templateUrl: "app/main/main.html",
-                controller: "MainController"
-            })
-			  .state('follow', {
-				url: "/follow-users",
-				templateUrl: "app/follow/follow.html",
-				controller: "FollowController"
-			})
+                    url: "/edit-profile",
+                    templateUrl: "app/profile/edit-profile-view.html",
+                    controller: "EditProfileController"
+                })
+                .state('main', {
+                    url: "/",
+                    templateUrl: "app/main/main.html",
+                    controller: "MainController"
+                })
+    			.state('follow', {
+    				url: "/follow-users",
+    				templateUrl: "app/follow/follow.html",
+    				controller: "FollowController"
+    			})
         })
 }());
